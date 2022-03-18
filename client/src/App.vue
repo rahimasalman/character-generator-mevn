038f633a8b8f3014d2f9dbe41a5b23cbbb539e90
Rahima Salmanova <rehimesalman7@gmail.com>
2022-03-18 10:16:05 +0400

    Add client side files

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <button v-on:click="toggle = 'character-viewer'">
+      View all characters
+    </button>
+    <button v-on:click="toggle = 'character-creator'">
+      Create a character
+    </button>
+    <CharacterViewer v-show="toggle === 'character-viewer'" />
+    <CharacterCreator v-show="toggle === 'character-creator'" />
+  </div>
+</template>
+
+<script>
+import CharacterViewer from "./components/CharacterViewer.vue";
+import CharacterCreator from "./components/CharacterCreator.vue";
+
+export default {
+  name: "App",
+  components: {
+    CharacterViewer,
+    CharacterCreator,
+  },
+  data: function () {
+    return {
+      toggle: "character-viewer",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/CharacterCreator.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="character-creator">
+    <h1>Character Creator</h1>
+    <label for="character-name"> Character Name: </label>
+    <input
+      id="character-name"
+      type="text"
+      v-model="name"
+      placeholder="Enter a name"
+    />
+    <br />
+    <br />
+    <label for="profession-list"> Character Profession: </label>
+    <select id="profession-list" v-model="profession">
+      <option value="Mage">Mage</option>
+      <option value="Thief">Thief</option>
+      <option value="Warrior">Warrior</option>
+    </select>
+    <p> {{name}}</p>
+    <p> {{profession}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CharacterCreator",
+  data: function () {
+    return {
+      name: "",
+      profession: "",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

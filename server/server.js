fd9d3d1b9e9049f3c5255b27d02b1fa2afe61027
Rahima Salmanova <rehimesalman7@gmail.com>
2022-03-18 15:47:53 +0400

    Configure database settings

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Database Connected Successfully!'));
+
 server.use(express.json());
 server.use(cors());
 

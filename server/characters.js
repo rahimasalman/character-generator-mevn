ade39bea071a2219685527df4d657bdc4babc35f
Rahima Salmanova <rehimesalman7@gmail.com>
2022-03-20 22:53:06 +0400

    Add characterSchema

--- server/characters.js
+++ server/characters.js
@@ -1,1 +1,14 @@
-const ad = "fferf"
+const express = require('express');
+const router = express.Router();
+const Character = require('./Character');
+
+router.get('/', async (req, res) => {
+    try {
+        const characters = await Character.find();
+        res.json(characters);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
